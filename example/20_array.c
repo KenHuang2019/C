6afe3108100b09c53515d832d3374ec963957dde
KenHuang <kenhuang2019iii@gmail.com>
2020-10-29 21:52:57 +0800

    update example 20

--- example/20_array.c
+++ example/20_array.c
@@ -6,19 +6,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define DIE_MAX_NUM 6 // 骰子只有六面是固定的，可用全域變數定義作為常數
+
+void printNum(int counter[])
+{
+   for (int j = 0; j < DIE_MAX_NUM; j++) // 這個 for loop 用來將每種點數出現的次數輸出
+   {
+      printf("Number %d: (normal)  %d\n", j+1, counter[j] ); // j+1是為了讓輸出的值不要從0開始，使用者讀起來較直觀
+   }
+}
 
 int main()
 {
    srand(time(NULL)); // seed
-   int counter[6] = {0}; // 用來放骰子點數出現次數的容器
+   int counter[DIE_MAX_NUM] = {0}; // 用來放骰子點數出現次數的容器
    for (int i = 0; i <=600; i++) // 擲骰子是個重複行為，而且知道終止條件是 5000 次，用 for loop 來寫 
    {
       int dice = rand() % 6; // 用亂數除上6之後取餘數，當作擲骰子的動作
       counter[dice]++; // 將 dice 直接作為讀取陣列的 index，用來記錄每種點數出現的次數
    }
    
-   for (int j = 0; j < 6; j++) // 這個 for loop 用來將每種點數出現的次數輸出
-   {
-      printf("Number %d: (normal)  %d\n", j+1, counter[j] ); // j+1是為了讓輸出的值不要從0開始，使用者讀起來較直觀
-   }
+   printNum(counter); // 傳遞陣列進別的函示只需要傳變數名稱，因為變數名稱就是陣列開頭的記憶體位置
 }
